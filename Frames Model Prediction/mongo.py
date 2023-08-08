7b24fa74413757a6b8b3a5aebfe22bd2389cb26f
HEET VEKAIRYA <heetvekariya50@gmail.com>
2023-08-08 20:24:24 +0530

    mongoDB intial set up

---
+++ Frames Model Prediction/mongo.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient 
+import base64
+from bson import ObjectId
+
+client = MongoClient('mongodb+srv://heetvekariya50:lF87e8KZEsVAC10Y@cluster0.yd7owd2.mongodb.net/')
+
+db = client['test']
+collection = db['temp']
+
+# Read the image as binary data
+with open('photo.png', 'rb') as image_file:
+    image_data = image_file.read()
+
+# Convert binary data to base64
+base64_image = base64.b64encode(image_data)
+
+# Insert the image data into MongoDB
+insert_id = collection.insert_one({"name": "Naimish", "image": base64_image})
+print(insert_id.inserted_id)
+
+# # retrieve the name from a document
+# retrieved_name = collection.find_one({"_id": ObjectId("64d21be959ea5968d3b601bf")})
+# print(retrieved_name)
+
+# # print all the fields in a document
+# for key in retrieved_name:
+#     print(key)
+
+
+
+
+
+
+# # retrieval of image from MongoDB
+# retrieved_image = collection.find_one({"_id": ObjectId('64d21ec219b95645ff88422b')})
+
+# # Convert the image data to binary
+# retrieved_image = base64.b64decode(retrieved_image['image'])
+
+# # Write the image data to a file
+# with open('retrieved_image.png', 'wb') as image_file:
+#     image_file.write(retrieved_image)
