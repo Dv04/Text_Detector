0745e2ddbe47a2d54c4fd9e2be8efc0dd11d0f9f
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-08-10 14:44:54 +0530

    Images are imported

--- ui/app.py
+++ ui/app.py
@@ -1,25 +1,34 @@
-from flask import Flask, render_template, request
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, request, url_for
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
-@app.route('/')
+img = os.path.join("image")
+file = os.path.join(img, "001.jpg")
+print(file)
+
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/frame', methods=['GET', 'POST'])
+
+@app.route("/frame", methods=["GET", "POST"])
 def frame():
-    if request.method == 'POST':
-        text = request.form.get('text')
-        updated = request.form.get('updated')
+    image_url = url_for("static", filename="image/020.jpg")  # Dynamic image URL
+    if request.method == "POST":
+        text = request.form.get("text")
+        updated = request.form.get("updated")
         print("text:", text)
         print("updated:", updated)
         # process the data using Python code
         # result = int(data) * 2
         # return str(result)
-    return render_template('index.html', predictedText="predicted text", image="image.jpg") # send here image
 
-    
+    return render_template(
+        "index.html", predictedText="predicted text", Image=image_url
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
