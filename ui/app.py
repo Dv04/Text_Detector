4c0458859966ef5a04cefdcde883f69f348adaef
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-08-10 16:28:21 +0530

    It is not working yet

--- ui/app.py
+++ ui/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session
 import os
+import secrets
+from datetime import datetime
 
-app = Flask(__name__, static_url_path="/static")
+# Generate a secure random secret key
+secret_key = secrets.token_hex(16)  # 16 bytes (128 bits) key
 
-img = os.path.join("image")
-file = os.path.join(img, "001.jpg")
-print(file)
+app = Flask(__name__)
+app.secret_key = secret_key
 
 
 @app.route("/")
@@ -15,7 +17,14 @@ def home():
 
 @app.route("/frame", methods=["GET", "POST"])
 def frame():
-    image_url = url_for("static", filename="image/020.jpg")  # Dynamic image URL
+    if "image_index" not in session:
+        session["image_index"] = 1
+
+    image_index = session["image_index"]
+    image_filename = f"{image_index:03d}.jpg"
+    image_url = url_for("static", filename=f"image/{image_filename}")
+    print(image_filename, image_index, image_url)
+
     if request.method == "POST":
         text = request.form.get("text")
         updated = request.form.get("updated")
@@ -25,8 +34,18 @@ def frame():
         # result = int(data) * 2
         # return str(result)
 
+        # Update image index for the next image
+        session["image_index"] = (image_index + 1) % 161  # Assuming 160 images
+        # After the session is updated
+        print("Session image index:", session["image_index"])
+
+    # Add this print statement
+    print("Image index:", image_url)
     return render_template(
-        "index.html", predictedText="predicted text", Image=image_url
+        "index.html",
+        predictedText="predicted text",
+        Image=image_url,
+        current_time=datetime.now().timestamp(),
     )
 
 
