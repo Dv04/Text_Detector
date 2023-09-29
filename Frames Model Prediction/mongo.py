09a9d7eab3f89a09d1089b91ec22deb1280625e3
Dev Sanghvi <62090715+Dv04@users.noreply.github.com>
2023-09-29 17:07:27 +0530

    Update mongo.py

--- Frames Model Prediction/mongo.py
+++ Frames Model Prediction/mongo.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import base64
 from bson import ObjectId
 
+# Your mongoDB cluster/localhost connection URL
 client = MongoClient('')
 
 db = client['test']
@@ -15,28 +16,13 @@ with open('photo.png', 'rb') as image_file:
 base64_image = base64.b64encode(image_data)
 
 # Insert the image data into MongoDB
-insert_id = collection.insert_one({"name": "Naimish", "image": base64_image})
-print(insert_id.inserted_id)
 
-# # retrieve the name from a document
-# retrieved_name = collection.find_one({"_id": ObjectId("64d21be959ea5968d3b601bf")})
-# print(retrieved_name)
+# retrieve the name from a document
 
-# # print all the fields in a document
-# for key in retrieved_name:
-#     print(key)
+# print all the fields in a document
 
+# retrieval of image from MongoDB
 
+# Convert the image data to binary
 
-
-
-
-# # retrieval of image from MongoDB
-# retrieved_image = collection.find_one({"_id": ObjectId('64d21ec219b95645ff88422b')})
-
-# # Convert the image data to binary
-# retrieved_image = base64.b64decode(retrieved_image['image'])
-
-# # Write the image data to a file
-# with open('retrieved_image.png', 'wb') as image_file:
-#     image_file.write(retrieved_image)
+# Write the image data to a file
